98eb3b21df0732fa71b3331379e54f3bbb68aadf
Geshata <64840882+g3shaa@users.noreply.github.com>
2023-02-17 13:46:13 +0200

    added task
    Игра - Познай числото

---
+++ Project_1_0_0/Project_1_0_0/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace SimpleGame.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Play(int guess)
+        {
+            int number = new Random().Next(1, 11);
+
+            if (guess == number)
+            {
+                ViewBag.Message = "Браво! Позна числото, което беше " + number;
+            }
+            else
+            {
+                ViewBag.Message = "Не позна! Числото беше " + number + ". Опитай пак!";
+            }
+
+
+            return View();
+        }
+    }
+}
---
+++ Project_1_0_0/Project_1_0_0/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Project_1_0_0.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
