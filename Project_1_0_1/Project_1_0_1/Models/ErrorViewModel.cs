bd3f06c5686fa724f8271a900b8764b227774188
Geshata <64840882+g3shaa@users.noreply.github.com>
2023-02-17 13:58:20 +0200

    added PACMAN - version 2.0 - ASP.NET MVC

---
+++ Project_1_0_1/Project_1_0_1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Project_1_0_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
